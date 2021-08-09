255c7fca2daa02ef5a053d7ccc2ca03b53d3204e
DavidCroix <cruzepedavid@gmail.com>
2021-08-09 14:52:48 -0700

    Se actualiza el archivo modulos.

--- modules/musiclife.py
+++ modules/musiclife.py
@@ -1,19 +1,76 @@
-import datetime
-import binascii
-from functools import wraps
-from os import environ, urandom
-import hashlib
-from bottle import response, request
-import jwt
-import models.auth as mauth
+import json
+from datetime import datetime
+from modules.storage import (
+    store_string,
+    store_bytes,
+    query_storage,
+    get_storage_file
+)
+def add_review(review_id = None, user_id = None, album_id = None, id_artist = None, rate = None, comment = None):
+    print("Desde Modulo add_review")
+    print(review_id, user_id, album_id, rate, comment)
+    print("Good")
 
-comments= []
-def add_comment(artist_id, album_id, rate):
-    comment = {
-        "album_id": artist_id,
+    almacenable = {
+        "review_id": review_id,
+        "user_id": user_id,
         "album_id": album_id,
+        "id_artist": id_artist,
         "rate": rate,
-        "user_id": user_id
+        "comment": comment,
     }
-    comments.append(comment)
-    return json.dumps(comment)
+    nombre_de_archivo = f"{album_id}-{review_id}-{id_artist}.json"
+    datos = store_string(
+        "album/reviews",
+        nombre_de_archivo,
+        json.dumps(almacenable)
+    )
+    return datos
+
+def get_reviews_from_album(album_id = None, reviews = None):
+    query_result = query_storage(
+        "album/reviews",
+    )
+    if album_id is not None:
+        return [
+           r
+           for r in query_result["content"]
+           if album_id in r
+        ]
+    print("Conejo Blas")
+
+def get_list_albums(albums=None):
+    query_result = query_storage(
+        "album/albums",
+    )
+    if albums is None:
+        return query_result["content"]
+
+def get_list_artists(artists=None):
+    query_result = query_storage(
+        "artist/artists",
+    )
+    if albums is None:
+        return query_result["content"]
+
+def update_album_details(review_id = None, user_id = None, album_id = None, id_artist = None, rate = None, comment = None ):
+    print("Desde Modulo store")
+    print(review_id, user_id, album_id, rate, comment)
+    print("Good")
+
+    almacenable = {
+        "review_id": review_id,
+        "user_id": user_id,
+        "album_id": album_id,
+        "id_artist": id_artist,
+        "rate": rate,
+        "comment": comment,
+    }
+    nombre_de_archivo = f"{album_id}-{id_artist}.json"
+    datos = store_string(
+        "album/albums",
+        nombre_de_archivo,
+        json.dumps(almacenable),
+        update=True
+    )
+    return datos
