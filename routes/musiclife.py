93d28b7f2dc5c6c24a2e1d2e03637b33e41365e7
DavidCroix <cruzepedavid@gmail.com>
2021-08-10 20:26:25 -0700

    Se eliminan comentarios

--- routes/musiclife.py
+++ routes/musiclife.py
@@ -11,12 +11,8 @@ from modules.musiclife import (
 
 app = BottleJson()
 
-@app.get("/")
-    #Default route
-
 ## Get albums list
 ## Curl example:
-# curl http://localhost:8080/movie/list -X GET
 @app.get("/list")
 def get_all_albums(*args, **kwargs):
     try:
@@ -27,7 +23,6 @@ def get_all_albums(*args, **kwargs):
 
 ## Get artists list
 ## Curl example:
-# curl http://localhost:8080/movie/list -X GET
 @app.get("/list")
 def get_all_artists(*args, **kwargs):
     try:
@@ -36,13 +31,9 @@ def get_all_artists(*args, **kwargs):
         raise bottle.HTTPError(500, "Error interno")
     raise bottle.HTTPError(200, respuesta)
 
-## Update movie details
+## Update albums details
 ## This works as a simple post. The store_string function
 ## updates de info that was previously storaged.
-## Curl example to update the Soy Leyenda movie information
-## curl http://localhost:8080/movie/M002 -X POST -H /
-##'Content-Type: application/json' -d /
-##'{"movie_id": "M002","title": "Soy Leyenda", "genre2": "Accion", "director": "Francis Lawrence", "release_date": "2008-01-18", "sinopsys": "Un cientifico lucha por sobrevivir contra unos mutantes nocturnos con sed de sangre."}'
 @app.post("/<movie_id>")
 def update_album_details(*args, **kwargs):
     payload = bottle.request.json
@@ -64,8 +55,6 @@ def update_album_details(*args, **kwargs):
     raise bottle.HTTPError(201, "Movie data has been updated")
 
 ## Add a review to a certain album
-# Example curl:
-# curl http://localhost:8080/movie/007/review -X POST -H 'Content-Type: application/json' -d '{"review_id": "002","user_id": "001", "movie_id": "007", "movie_title": "Inception", "rate": "5", "comment": "goooood bro ngl"}'
 @app.post("/<movie_id>/review")
 def bar(*args, **kwargs):
     payload = bottle.request.json
@@ -84,9 +73,7 @@ def bar(*args, **kwargs):
         raise bottle.HTTPError(400)
     raise bottle.HTTPError(201, "Your review has been succesfully added")
 
-## Get all reviews from a movie
-## Curl example:
-# curl http://localhost:8080/movie/M001/reviews -X GET
+## Get all reviews from an album
 @app.get("/<movie_id>/reviews")
 def get_all_reviews_from_movie(*args, album_id=None, **kwargs):
     try:
