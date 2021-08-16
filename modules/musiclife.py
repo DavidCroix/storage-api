6451e9b446d6f52144f5f5ac2f967d98ac71b0a3
DavidCroix <cruzepedavid@gmail.com>
2021-08-15 17:51:48 -0700

    No se como solucionar el que el def no se declare correctamente.

--- modules/musiclife.py
+++ modules/musiclife.py
@@ -79,6 +79,25 @@ def update_album_details(review_id = None, user_id = None, album_id = None, id_a
 
 ## Extension para ver si funciona
 
+## add_artist
+
+    def add_artist(id_artist = None, genre = None):
+    print("Desde Modulo add_artist")
+    print(id_artist, genre)
+    print("Exito")
+
+    almacenable = {
+        "id_artist": id_artist,
+        "genre"; genre,
+    }
+    nombre_de_archivo = f"{id_artist}.json"
+    datos_artist = storage_string(
+        "artist/artists",
+        nombre_de_archivo,
+        json.dumps(almacenable)
+    )
+    return datos_artist
+
 ## add_album
 def add_album(id_artist = None, album_id = None, genre = None):
 
@@ -97,22 +116,4 @@ def add_album(id_artist = None, album_id = None, genre = None):
         nombre_de_archivo,
         json.dumps(almacenable)
     )
-    return datos_album
-
-    def add_album(id_artist = None, album_id = None, genre = None):
-
-    print("Desde Modulo add_artist")
-    print(id_artist, genre)
-    print("Exito")
-
-    almacenable = {
-        "id_artist": id_artist,
-        "genre"; genre,
-    }
-    nombre_de_archivo = f"{id_artist}.json"
-    datos_artist = storage_string(
-        "artist/artists",
-        nombre_de_archivo,
-        json.dumps(almacenable)
-    )
-    return datos_artist
+    return [datos_album]
